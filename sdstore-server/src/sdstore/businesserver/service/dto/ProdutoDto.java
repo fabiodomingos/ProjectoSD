4467562a7ae1125e13459a699fb9e60e0832488a
Mimoso <Mimoso@172.20.20.10>
2012-10-30 13:09:15 +0000

    produtos dto

--- sdstore-server/src/sdstore/businesserver/service/dto/ProdutoDto.java
+++ sdstore-server/src/sdstore/businesserver/service/dto/ProdutoDto.java
@@ -1,5 +1,38 @@
 package sdstore.businesserver.service.dto;
 
 public class ProdutoDto {
+	
+	private String categoria;
+	private String codigo;
+	private String descricao;
+	
+	public ProdutoDto(String categoria, String codigo, String descricao) {
+		this.categoria = categoria;
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
 
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 }
