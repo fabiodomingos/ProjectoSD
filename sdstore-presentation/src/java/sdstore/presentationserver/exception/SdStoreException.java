6367537606706ed116c8b32d1db813d13f2eb921
Diogo <Diogo@Diogo-PC>
2012-10-30 11:35:37 +0000

    novo

--- sdstore-presentation/src/java/sdstore/presentationserver/exception/SdStoreException.java
+++ sdstore-presentation/src/java/sdstore/presentationserver/exception/SdStoreException.java
@@ -3,7 +3,7 @@ package sdstore.presentationserver.exception;
 public class SdStoreException extends RuntimeException{
 
 	/**
-	 * 
+	 * olaaaaa
 	 */
 	private static final long serialVersionUID = 1L;
 
