a1b0a20317b9a15b9ff88866a33f859371f04288
Diogo <Diogo@Diogo-PC>
2012-10-30 14:31:05 +0000

    produto

--- sdstore-server/src/sdstore/businesserver/domain/Produto.java
+++ sdstore-server/src/sdstore/businesserver/domain/Produto.java
@@ -1,10 +1,39 @@
 package sdstore.businesserver.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import anacom.businessserver.domain.Operator;
+import anacom.businessserver.exception.OperatorPrefixException;
+
 public class Produto {
+	
+	private static Map<String, Produto> produtoMap;
+	
+	static {
+		Produto.produtoMap = new HashMap<String, Produto>();
+	}
 	
 	String id;
 	String descricao;
 	String categoria;
+	
+	public static void createProduto(String id, String descricao, String categoria) throws ProdutoIdException {
+		Produto produto = Produto.produtoMap.get(id);
+		if (produto != null) {
+			throw new ProdutoIdException(id);
+		}
+		Produto newProduto = new Produto(id, descricao, categoria);
+		Produto.produtoMap.put(id, newProduto);
+	}
+	
+	public static Produto getProduto(String id) throws ProdutoIdException {
+		Produto produto = Produto.produtoMap.get(id);
+		if (produto == null) {
+			throw new ProdutoIdException(id);
+		}
+		return Produto.produtoMap.get(id);
+	}
 	
 	public Produto(String id, String descricao, String categoria) {
 		this.id = id;
