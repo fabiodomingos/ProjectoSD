1db97e43691559352fdfeffab2f73881727e0bfd
Mimoso <Mimoso@172.20.20.10>
2012-10-30 16:34:47 +0000

    excecoes

---
+++ sdstore-server/src/sdstore/businesserver/exception/FornecedorProdutoListException.java
@@ -0,0 +1,23 @@
+package sdstore.businesserver.exception;
+
+public class FornecedorProdutoListException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public FornecedorProdutoListException(String nome) {
+		this.nome = nome;
+	}
+}
