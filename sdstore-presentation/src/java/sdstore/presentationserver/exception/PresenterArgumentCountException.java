aba4ee6ba0fd2b02e96138819966b050583e7fcf
Mimoso <Mimoso@172.20.20.10>
2012-10-30 11:52:52 +0000

    novas

--- sdstore-presentation/src/java/sdstore/presentationserver/exception/PresenterArgumentCountException.java
+++ sdstore-presentation/src/java/sdstore/presentationserver/exception/PresenterArgumentCountException.java
@@ -1,5 +1,34 @@
 package sdstore.presentationserver.exception;
 
-public class PresenterArgumentCountException {
+public class PresenterArgumentCountException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String command;
+	private Integer invalidArg;
+	
+	public PresenterArgumentCountException(String command, Integer invalidArg) {
+		this.command = command;
+		this.invalidArg = invalidArg;
+	}
+
+	public String getCommand() {
+		return command;
+	}
+
+	public void setCommand(String command) {
+		this.command = command;
+	}
+
+	public Integer getInvalidArg() {
+		return invalidArg;
+	}
+
+	public void setInvalidArg(Integer invalidArg) {
+		this.invalidArg = invalidArg;
+	}
+	
 }
