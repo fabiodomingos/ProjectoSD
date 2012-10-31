f040ea00d1ddde5db46dfa2ec787b9ad92ee0b63
Mimoso <Mimoso@Mimoso-PC>
2012-10-31 12:09:35 +0000

    xml dos fornecedores

--- sdstore-server/src/java/sdstore/businesserver/BusinessServerInitListener.java
+++ sdstore-server/src/java/sdstore/businesserver/BusinessServerInitListener.java
@@ -1,5 +1,27 @@
 package sdstore.businesserver;
 
-public class BusinessServerInitListener {
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 
+import sdstore.businesserver.domain.Fornecedor;
+
+
+public class BusinessServerInitListener implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0){
+		//codigo para libertar recursos antes do undeploy
+	}
+	
+	@Override 
+	public void contextInitialized(ServletContextEvent arg0){
+		//codigo para executar apos o deploy da aplicacao no jboss
+		try {
+			String fornecedorName = arg0.getServletContext().getInitParameter("fornecedorName");
+			Fornecedor.createFornecedor(fornecedorName);
+		} catch (Exception e) {
+			System.out.println("Erro ao iniciar a webapp\n");
+			e.printStackTrace();
+		}
+	}
 }
