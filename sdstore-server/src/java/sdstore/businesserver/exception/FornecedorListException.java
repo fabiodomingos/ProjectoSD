ed3d5a39ae44486ea7032d180d155aab3628004a
Diogo <Diogo@Diogo-PC>
2012-10-31 14:57:48 +0000

    nova excepcao

---
+++ sdstore-server/src/java/sdstore/businesserver/exception/FornecedorListException.java
@@ -0,0 +1,15 @@
+package sdstore.businesserver.exception;
+
+public class FornecedorListException extends Exception {
+	
+private static final long serialVersionUID = 1L;
+	
+	
+	
+	public FornecedorListException() {
+		super();
+	}
+
+	
+
+}
