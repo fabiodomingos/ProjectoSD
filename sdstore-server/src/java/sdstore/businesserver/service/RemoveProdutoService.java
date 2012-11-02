4d9ae1d7387ef3389a439198c67dcd83ff04d87d
Diogo <Diogo@Diogo-PC>
2012-11-02 12:56:48 +0000

    novo service (remove prod

--- sdstore-server/src/java/sdstore/businesserver/service/RemoveProdutoService.java
+++ sdstore-server/src/java/sdstore/businesserver/service/RemoveProdutoService.java
@@ -1,17 +1,37 @@
 package sdstore.businesserver.service;
 
+import sdstore.businesserver.domain.Fornecedor;
+import sdstore.businesserver.domain.Produto;
+import sdstore.businesserver.exception.FornecedorNameException;
+import sdstore.businesserver.service.dto.FornecedorProdutoRegisterDto;
 import sdstore.businesserver.service.dto.ProdutoDto;
 
-public class RemoveProdutoService {
+public class RemoveProdutoService extends PortalService {
 
-	public ProdutoDto getProduto() {
-		// TODO Auto-generated method stub
-		return null;
+	private FornecedorProdutoRegisterDto produtoDto;
+	
+	public RemoveProdutoService(FornecedorProdutoRegisterDto dto){
+		this.produtoDto = dto;
+		
 	}
-
-	public ProdutoDto getProduto() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public final void dispatch() throws FornecedorNameException{
+		try {			
+			Integer total;
+			String codigo = produtoDto.getProduto().getCodigo();
+			String nomeFornecedor = produtoDto.getProduto().getFornecedor().getNome();
+			Fornecedor forn = Fornecedor.getFornecedor(nomeFornecedor);
+			Produto p = forn.getProduto(codigo);
+			total = forn.getNumeroProdutos(p);
+			forn.getProduto(codigo).setQuantidade(total-1);
+			forn.unregisterProduto(codigo);
+		}catch(FornecedorNameException e){
+			throw e;
+		}
+	}
+	
+	public FornecedorProdutoRegisterDto getProduto() {
+		return produtoDto;
 	}
 
 }
