c4676c4ee1df79a731ade912458a5990f39177e2
Diogo <Diogo@Diogo-PC>
2012-11-02 20:17:49 +0000

    CategoriaListService feito

--- sdstore-server/src/java/sdstore/businesserver/service/ListaCategoriaService.java
+++ sdstore-server/src/java/sdstore/businesserver/service/ListaCategoriaService.java
@@ -1,12 +1,50 @@
 package sdstore.businesserver.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import sdstore.businesserver.domain.Fornecedor;
+import sdstore.businesserver.domain.Produto;
+import sdstore.businesserver.exception.CategoriaListException;
+import sdstore.businesserver.exception.CategoriaNameException;
+import sdstore.businesserver.service.dto.CategoriaDto;
 import sdstore.businesserver.service.dto.CategoriaListDto;
+import sdstore.businesserver.service.dto.FornecedorSelectionDto;
 
 public class ListaCategoriaService {
+	
+	CategoriaListDto result;
+	FornecedorSelectionDto dto;
+	
+	
+	public ListaCategoriaService(FornecedorSelectionDto dto) {
+		this.dto = dto;
+	}
+	
+	public final void dispatch() throws CategoriaNameException, CategoriaListException {
+		try {		
+			String nomeFornecedor = dto.getNome();
+			String categoria;
+			Fornecedor forn = Fornecedor.getFornecedor(nomeFornecedor);
+			List<Produto> listaProdutosFornecedor = forn.getProdutoList();
+			List<CategoriaDto> categoriaList = new ArrayList<CategoriaDto>();
+			for(Produto prod : listaProdutosFornecedor){
+				categoria = prod.getCategoria();
+				CategoriaDto categoriaDto = new CategoriaDto(categoria);
+				categoriaList.add(categoriaDto);
+			}
+			result = new CategoriaListDto(categoriaList);
+		} catch (CategoriaNameException exception) {
+			throw exception;
+		} catch (CategoriaListException exception) {
+			throw exception;
+		}
+	}
+
+
 
 	public CategoriaListDto getListCategoria() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.result;
 	}
 
 }
