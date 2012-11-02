6e8526260cc8dd25789be2629a32cedfc44dcbb1
Mimoso <Mimoso@Mimoso-PC>
2012-11-02 14:30:52 +0000

    Carrinho

--- sdstore-server/src/java/sdstore/businesserver/domain/Carrinho.java
+++ sdstore-server/src/java/sdstore/businesserver/domain/Carrinho.java
@@ -3,14 +3,32 @@ package sdstore.businesserver.domain;
 import java.util.HashMap;
 import java.util.Map;
 
+import sdstore.businesserver.exception.ClienteNomeException;
+
 public class Carrinho {
+
+	private static Map<String, Carrinho> carrinhoMap;
+	
+	static {
+		Carrinho.carrinhoMap = new HashMap<String, Carrinho>();
+	}
 	
 	private String id_cliente;
 
-	private static Map<Integer, Produto> carrinhoMap;
+	public static void createCarrinho(String cliente) throws ClienteNomeException{
+		Carrinho carrinho = Carrinho.carrinhoMap.get(cliente);
+		if(carrinho == null){
+			throw new ClienteNomeException(cliente);
+		}
+		Carrinho newCarrinho = new Carrinho(cliente);
+		Carrinho.carrinhoMap.put(cliente, newCarrinho);	
+	}
 	
-	public Carrinho() {
-		carrinhoMap = new HashMap<Integer, Produto>();
+	
+	
+	public Carrinho(String cliente) {
+		this.id_cliente = cliente;
+		carrinhoMap = new HashMap<String, Carrinho>();
 	}
 
 	public String getId_cliente() {
