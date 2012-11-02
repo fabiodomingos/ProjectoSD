088e59a0803b2e9791b221145092f937ccdb5ab3
Diogo <Diogo@Diogo-PC>
2012-11-02 12:20:14 +0000

    Novo Dto criado

--- sdstore-server/src/java/sdstore/businesserver/service/dto/ClienteSelectionDto.java
+++ sdstore-server/src/java/sdstore/businesserver/service/dto/ClienteSelectionDto.java
@@ -1,5 +1,19 @@
 package sdstore.businesserver.service.dto;
 
 public class ClienteSelectionDto {
+	
+	private String nome;
+	
+	public ClienteSelectionDto(String nome){
+		this.nome = nome;
+	}
+	
+	public String getNome(){
+		return nome;
+	}
+	
+	public void setNome(String nome){
+		this.nome = nome;
+	}
 
 }
