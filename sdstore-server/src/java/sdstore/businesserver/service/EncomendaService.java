4b5e807e90763aaf5d7b151ca9dfb4fc7dde52dd
Diogo <Diogo@Diogo-PC>
2012-11-02 12:19:02 +0000

    servico incompleto (encomendaService)

--- sdstore-server/src/java/sdstore/businesserver/service/EncomendaService.java
+++ sdstore-server/src/java/sdstore/businesserver/service/EncomendaService.java
@@ -1,12 +1,37 @@
 package sdstore.businesserver.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import sdstore.businesserver.domain.Cliente;
+import sdstore.businesserver.domain.Produto;
+import sdstore.businesserver.exception.ProdutoIdException;
+import sdstore.businesserver.service.dto.FornecedorProdutoRegisterDto;
+import sdstore.businesserver.service.dto.FornecedorSelectionDto;
 import sdstore.businesserver.service.dto.ProdutoDto;
+import sdstore.businesserver.service.dto.ProdutoListDto;
 
-public class EncomendaService {
+public class EncomendaService extends PortalService {
+	
+	private ProdutoListDto carrinho;
+	private FornecedorProdutoRegisterDto produtoFornecedor;
+	private FornecedorSelectionDto fornecedor;
+	
+	
+	public EncomendaService(ProdutoListDto dto){
+		this.carrinho = dto;
+	}
+	
+	public final void dispatch() throws ProdutoIdException {
+		try {			
+			String nomeFornecedor = fornecedor.getNome();
+		} catch (ProdutoIdException exception) {
+			throw exception;
+		}
+	}
 
-	public ProdutoDto getEncomenda() {
-		// TODO Auto-generated method stub
-		return null;
+	public ProdutoListDto getEncomenda() {
+		return carrinho;
 	}
 
 }
