e861575a92cab016bc8ee559fd2a7fbec6c43473
Diogo <Diogo@Diogo-PC>
2012-11-02 12:18:09 +0000

    novo servico (addProduto)

--- sdstore-server/src/java/sdstore/businesserver/service/AddProdutoService.java
+++ sdstore-server/src/java/sdstore/businesserver/service/AddProdutoService.java
@@ -1,18 +1,37 @@
 package sdstore.businesserver.service;
 
+import sdstore.businesserver.domain.Fornecedor;
+import sdstore.businesserver.domain.Produto;
+import sdstore.businesserver.exception.FornecedorNameException;
+import sdstore.businesserver.service.dto.FornecedorProdutoRegisterDto;
 import sdstore.businesserver.service.dto.ProdutoDto;
 
 public class AddProdutoService extends PortalService{
 	
-	private ProdutoDto produto;
-	private 
+	private FornecedorProdutoRegisterDto produtoDto;
 	
-	public AddProdutoService(ProdutoDto produto){
-		
+	
+	public AddProdutoService(FornecedorProdutoRegisterDto produto){
+		this.produtoDto = produto;
 	}
-	public ProdutoDto getProduto() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public final void dispatch() throws FornecedorNameException{
+		try {			
+			String codigo = produtoDto.getNewProduto().getCodigo();
+			String categoria = produtoDto.getNewProduto().getCategoria();
+			String descricao = produtoDto.getNewProduto().getDescricao();
+			String nomeFornecedor = produtoDto.getNewProduto().getFornecedor().getNome();
+			Fornecedor forn = Fornecedor.getFornecedor(nomeFornecedor);
+			Produto novo = new Produto(codigo, descricao, categoria);
+			forn.registerProduto(novo, produtoDto.getPrizeProduto(), produtoDto.getTotalProduto());
+		}catch(FornecedorNameException e){
+			throw e;
+		}
+	}
+	
+	
+	public FornecedorProdutoRegisterDto getProduto() {
+		return produtoDto;
 	}
 
 }
