6b6732d40fbface6e9b8f8fc88d2e12ff70055cf
Diogo <Diogo@Diogo-PC>
2012-11-05 15:30:10 +0000

    Novo servico feito: lista produtos por categoria do catalogo

--- sdstore-server/src/java/sdstore/businesserver/service/ListaProdutoService.java
+++ sdstore-server/src/java/sdstore/businesserver/service/ListaProdutoService.java
@@ -1,30 +1,53 @@
 package sdstore.businesserver.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import sdstore.businesserver.domain.Catalogo;
+import sdstore.businesserver.domain.Produto;
+import sdstore.businesserver.exception.CatalogoNameException;
+import sdstore.businesserver.exception.ProdutoListException;
+import sdstore.businesserver.service.dto.CatalogoDto;
+import sdstore.businesserver.service.dto.CategoriaDto;
 import sdstore.businesserver.service.dto.ProdutoDto;
 import sdstore.businesserver.service.dto.ProdutoListDto;
 
 
 public class ListaProdutoService extends PortalService{
 
-	ProdutoDto categoriaDto;
+	CategoriaDto categoriaDto;
+	CatalogoDto catalogoDto;
+	ProdutoListDto result;
 	
-	public ListaProdutoService(ProdutoDto categoria) {
+	public ListaProdutoService(CategoriaDto categoria, CatalogoDto dto) {
 		this.categoriaDto = categoria;
-	}
-
-	public ProdutoListDto getListProduto() {
-		// TODO Auto-generated method stub
-		return null;
+		this.catalogoDto = dto;
 	}
 
 	@Override
-	public final void dispatch() {
+	public final void dispatch() throws ProdutoListException, CatalogoNameException {
 		try{
+			String categoria = categoriaDto.getNome();
+			String nomeCatalogo = catalogoDto.getNome();
+			Catalogo cata = Catalogo.getCatalogo(nomeCatalogo);
+			List<Produto> listaProdutosCategoria = cata.getProdutosCategoria(categoria);
+			List<ProdutoDto> listaDto = new ArrayList<ProdutoDto>();
+			for(Produto prod : listaProdutosCategoria){
+				ProdutoDto aux = new ProdutoDto(prod.getCategoria(), prod.getId(), prod.getDescricao());
+				listaDto.add(aux);
+			}
+			result = new ProdutoListDto(listaDto);
 			
-		}catch{
-			
+		}catch(ProdutoListException exception){
+			throw exception;
+		}catch(CatalogoNameException exception){
+			throw exception;
 		}
 		
+	}
+	
+	public ProdutoListDto getListProduto() {
+		return this.result;
 	}
 
 }
