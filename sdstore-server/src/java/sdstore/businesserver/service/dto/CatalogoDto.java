caa6a6137f3fc263f8eb14d25f3094b45817f88a
Diogo <Diogo@Diogo-PC>
2012-11-05 15:30:58 +0000

    Novo Dto: catalogoDto

---
+++ sdstore-server/src/java/sdstore/businesserver/service/dto/CatalogoDto.java
@@ -0,0 +1,19 @@
+package sdstore.businesserver.service.dto;
+
+public class CatalogoDto {
+
+	String nome;
+
+	public CatalogoDto(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
