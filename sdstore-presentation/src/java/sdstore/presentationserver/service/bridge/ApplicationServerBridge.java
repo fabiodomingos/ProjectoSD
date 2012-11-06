3cc940ddc959f5527b4a4fa1921e2c2ab0cda270
Mimoso <Mimoso@172.20.20.61>
2012-11-06 12:11:10 +0000

    application server bridge

--- sdstore-presentation/src/java/sdstore/presentationserver/service/bridge/ApplicationServerBridge.java
+++ sdstore-presentation/src/java/sdstore/presentationserver/service/bridge/ApplicationServerBridge.java
@@ -2,4 +2,15 @@ package sdstore.presentationserver.service.bridge;
 
 public interface ApplicationServerBridge{
 
+//	public void Encomenda(ClienteSelectionDto dto);
+	
+//	public void Limpa(ClienteSelectionDto dto);
+	
+//	public void Junta(ProdutoSelectDto dto);
+	
+//	public ProdutoListDto Carrinho(ClienteSelectionDto dto);
+	
+//	public ProdutoListDto ListaProdutosCategoria(CategoriaDto dto);
+	
+//	public CategoriaListDto ListaCategoria(ClienteSelectionDto dto);
 }
