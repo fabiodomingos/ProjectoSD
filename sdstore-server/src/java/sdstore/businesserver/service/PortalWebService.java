0111ee5e4f0de850371c891d756c4a947b9beffd
Mimoso <Mimoso@172.20.20.61>
2012-11-06 22:38:47 +0000

    PortalWebService

--- sdstore-server/src/java/sdstore/businesserver/service/PortalWebService.java
+++ sdstore-server/src/java/sdstore/businesserver/service/PortalWebService.java
@@ -5,41 +5,37 @@ import javax.jws.WebService;
 
 import sdstore.businesserver.exception.ProdutoListException;
 import sdstore.businesserver.service.dto.CategoriaListDto;
+import sdstore.businesserver.service.dto.ProdutoDto;
+import sdstore.businesserver.service.dto.ProdutoListDto;
 
 @WebService
 public class PortalWebService {
 	
-//	@WebMethod
-//	public FornecedorProdutoRegisterDto AddProdutoWebService(FornecedorProdutoRegisterDto dto) throws FornecedorNameException{
-//		AddProdutoService service = new AddProdutoService(dto);
-//		service.execute();
-//		FornecedorProdutoRegisterDto result = service.getProduto();
-//		return result;
-//	}
+	@WebMethod
+	public String RetiraProduto(String codigo,Integer quantidade){
+		RetiraProdutoService service = new RetiraProdutoService(codigo,quantidade);
+		service.execute();
+		String result = service.getResultado();
+		return result;
+	}
 	
-//	@WebMethod
-//	public ProdutoDto RemoveProdutoWebService(ProdutoSelectionDto dto){
-//		RemoveProdutoService service = new RemoveProdutoService(dto);
-//		service.execute();
-//		ProdutoDto result = service.getProduto();
-//		return result;
-//	}
 	
-//	@WebMethod
-//	public void JuntaWebService(ProdutoDto dto, Integer quantidade, ClienteSelectionDto cliente) throws ProdutoIdException, ClienteNomeException, FornecedorProdutoExistsException{
-//		JuntaService service = new JuntaService(dto,quantidade,cliente);
-//		service.execute();
-//		//ProdutoDto result = service.getProduto();
-////		return result;
-//	}
+	@WebMethod
+	public ProdutoDto PedeProduto(String codigo,Integer quantidade){
+		PedeProdutoService service = new PedeProdutoService(codigo,quantidade);
+		service.execute();
+		ProdutoDto result = service.getProdutoDto();
+		return result;
+	}
+
 	
-//	@WebMethod
-//	public ProdutoListDto ListaProdutoWebService(CategoriaDto dto, String nomeCatalogo) throws ProdutoListException, CatalogoNameException{
-//		ListaProdutoService service = new ListaProdutoService(dto, nomeCatalogo);
-//		service.execute();
-//		ProdutoListDto result = service.getListProduto();
-//		return result;
-//	}
+	@WebMethod
+	public ProdutoListDto ListaProdutoWebService(String categoria){
+		ListaProdutoService service = new ListaProdutoService(categoria);
+		service.execute();
+		ProdutoListDto result = service.getListProdutos();
+		return result;
+	}
 	
 	@WebMethod
 	public CategoriaListDto ListaCategoriaWebService() {
@@ -48,29 +44,4 @@ public class PortalWebService {
 		CategoriaListDto result = service.getListCategoria();
 		return result;	
 	}
-	
-//	@WebMethod
-//	public ProdutoListDto CarrinhoWebService(ClienteSelectionDto dto){
-//		CarrinhoService service = new CarrinhoService(dto);
-//		service.execute();
-//		ProdutoListDto result = service.getCarrinho();
-//		return result;
-//	}
-//	
-//	@WebMethod
-//	public void LimpaWebService(ClienteSelectionDto dto) throws ClienteNomeException{
-//		LimpaService service = new LimpaService(dto);
-//		service.execute();
-//	}
-//	
-//	@WebMethod
-//	public ProdutoDto EncomendaWebService(ProdutoSelectionDto dto){
-//		EncomendaService service = new EncomendaService(dto);
-//		service.execute();
-//		ProdutoDto result = service.getEncomenda();
-//		return result;
-//	}
-//	
-	
-
 }
