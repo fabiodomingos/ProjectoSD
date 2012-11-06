950772641e524dda5f9e8579d8f3f4a482385d55
Mimoso <Mimoso@172.20.20.61>
2012-11-06 12:10:44 +0000

    lista produtos presenter

--- sdstore-presentation/src/java/sdstore/presentationserver/presenter/ListaProdutosPresenter.java
+++ sdstore-presentation/src/java/sdstore/presentationserver/presenter/ListaProdutosPresenter.java
@@ -2,4 +2,10 @@ package sdstore.presentationserver.presenter;
 
 public class ListaProdutosPresenter {
 
+	public static void present(ProdutoListDto dto){
+		List<ProdutoDto> produtoList = dto.getProductList();
+		for(ProdutoDto produtoDto : produtoList){
+//			System.out.println(produtoDto);
+		}
+	}
 }
