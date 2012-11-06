3450e71e236887cfd976ecfd628a3e802cb1cca2
Mimoso <Mimoso@172.20.20.61>
2012-11-06 12:11:26 +0000

    remote application server

--- sdstore-presentation/src/java/sdstore/presentationserver/service/bridge/RemoteApplicationServer.java
+++ sdstore-presentation/src/java/sdstore/presentationserver/service/bridge/RemoteApplicationServer.java
@@ -1,5 +1,20 @@
 package sdstore.presentationserver.service.bridge;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RemoteApplicationServer implements ApplicationServerBridge{
 
+	private static Map<String,String> endpointUrlMap;
+	
+	static{
+		RemoteApplicationServer.endpointUrlMap = new HashMap<String,String>();
+		RemoteApplicationServer.endpointUrlMap.put("Portal", "http://localhost:8080/sdstore-server/blabla?wsdl");
+	}
+	
+//	PortalWebService webService:
+//	{
+//		PortalWebServiceService servico = new PortalWebServiceService();
+//		webService = servico.getPortalWebServicePort();
+//	}
 }
