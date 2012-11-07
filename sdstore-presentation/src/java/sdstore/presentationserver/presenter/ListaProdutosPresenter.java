33b42d86c2a4e141c03add9db82c40fabde508c4
Mimoso <Mimoso@172.20.20.61>
2012-11-07 12:14:28 +0000

    ListaProdutosPresenter

--- sdstore-presentation/src/java/sdstore/presentationserver/presenter/ListaProdutosPresenter.java
+++ sdstore-presentation/src/java/sdstore/presentationserver/presenter/ListaProdutosPresenter.java
@@ -1,11 +1,23 @@
 package sdstore.presentationserver.presenter;
 
+import java.util.List;
+
+import sdstore.presentationserver.service.stubs.ProdListDto;
+import sdstore.presentationserver.service.stubs.ProdutoDto;
+
 public class ListaProdutosPresenter {
 
-	public static void present(ProdutoListDto dto){
-		List<ProdutoDto> produtoList = dto.getProductList();
+	public static void present(ProdListDto dto){
+		List<ProdutoDto> produtoList = dto.getListaDto();
+		System.out.println("akiii");
+		System.out.println(produtoList);
 		for(ProdutoDto produtoDto : produtoList){
-//			System.out.println(produtoDto);
+			System.out.println("estouuuuuu");
+			System.out.println(produtoDto.getId().toString()+" "
+		+produtoDto.getCategoria()+" "
+		+produtoDto.getDescricao()+" "
+		+produtoDto.getPreco()+ " "
+		+produtoDto.getQuantidade());
 		}
 	}
 }
