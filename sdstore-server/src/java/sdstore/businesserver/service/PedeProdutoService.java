09e7b33de1899142f9301a1b51193d430519142e
Mimoso <Mimoso@172.20.20.61>
2012-11-07 19:16:32 +0000

    pede produto service refactoring

--- sdstore-server/src/java/sdstore/businesserver/service/PedeProdutoService.java
+++ sdstore-server/src/java/sdstore/businesserver/service/PedeProdutoService.java
@@ -1,6 +1,5 @@
 package sdstore.businesserver.service;
 
-import java.util.List;
 
 import sdstore.businesserver.domain.Catalogo;
 import sdstore.businesserver.domain.Produto;
@@ -17,40 +16,16 @@ public class PedeProdutoService extends PortalService{
 	
 	@Override
 	public final void dispatch() {
-//		System.out.println("Aqui entrei");
-//		List<Produto> lista = Catalogo.getProdutoList();
-//		
-//		for(Produto prod : lista){
-//			System.out.println("Agarreiiiiiiiiii "+prod.getId());
-//			if(prod.getId().equals(_codigo)){
-//				System.out.println("aquiiiiiiiiiiiiiiiiiii"+prod.getId());
-//				result.setId(prod.getId());
-//				result.setQuantidade(prod.getQuantidade());
-//				result.setPreco(prod.getPreco());
-//				result.setCategoria(prod.getCategoria());
-//				result.setDescricao(prod.getDescricao());
-//			}
-//		}
-		
 		Produto prodPedido = Catalogo.getProduto(_codigo);
-		System.out.println(prodPedido);
-		System.out.println("IDPEDIDO "+prodPedido.getId());
 		ProdutoDto resultado = new ProdutoDto();
-		System.out.println("passei");
 		resultado.setId(prodPedido.getId());
-//		System.out.println("ENVIA "+result.getId());
 		resultado.setQuantidade(prodPedido.getQuantidade());
-//		System.out.println("ENVIA "+result.getQuantidade());
 		resultado.setPreco(prodPedido.getPreco());
-//		System.out.println("ENVIA "+result.getPreco());
 		resultado.setCategoria(prodPedido.getCategoria());
-//		System.out.println("ENVIA "+result.getCategoria());
 		resultado.setDescricao(prodPedido.getDescricao());
-//		System.out.println("ENVIA "+result.getDescricao());
 		result = resultado;
 	}
 	
-
 	public ProdutoDto getProdutoDto(){
 		return this.result;
 	}
