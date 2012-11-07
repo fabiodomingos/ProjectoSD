025214331826a839e56e42c1fbcb0f3271801ac7
Mimoso <Mimoso@172.20.20.61>
2012-11-07 12:14:01 +0000

    Remote Server

--- sdstore-presentation/src/java/sdstore/presentationserver/service/bridge/RemoteApplicationServer.java
+++ sdstore-presentation/src/java/sdstore/presentationserver/service/bridge/RemoteApplicationServer.java
@@ -3,18 +3,50 @@ package sdstore.presentationserver.service.bridge;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.xml.ws.BindingProvider;
+
+import sdstore.presentationserver.service.stubs.CategoriaListDto;
+import sdstore.presentationserver.service.stubs.ConsolaWebService;
+import sdstore.presentationserver.service.stubs.ConsolaWebServiceService;
+import sdstore.presentationserver.service.stubs.ProdListDto;
+
 public class RemoteApplicationServer implements ApplicationServerBridge{
 
 	private static Map<String,String> endpointUrlMap;
 	
 	static{
 		RemoteApplicationServer.endpointUrlMap = new HashMap<String,String>();
-		RemoteApplicationServer.endpointUrlMap.put("Portal", "http://localhost:8080/sdstore-server/blabla?wsdl");
+		RemoteApplicationServer.endpointUrlMap.put("Portal", "http://localhost:8080/sdstore-portal/Portal?wsdl");
 	}
 	
-//	PortalWebService webService:
-//	{
-//		PortalWebServiceService servico = new PortalWebServiceService();
-//		webService = servico.getPortalWebServicePort();
-//	}
+	ConsolaWebService webService;
+	{
+		ConsolaWebServiceService servico = new ConsolaWebServiceService();
+		webService = servico.getConsolaWebServicePort();
+	}
+	
+	private void updateEndpointUrl(){
+		String endpointUrl = RemoteApplicationServer.endpointUrlMap.get("Portal");
+		if (endpointUrl == null) {
+//			throw new OperatorPrefixException_Exception();
+		}
+		BindingProvider bp = (BindingProvider)webService;
+		bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, endpointUrl);
+	}
+
+	@Override
+	public CategoriaListDto listaCategoria() {
+		updateEndpointUrl();
+		CategoriaListDto resultado = webService.listaCategoriaWebService();
+		return resultado;
+	}
+
+	@Override
+	public ProdListDto listaProdutosCategoria(String categoria) {
+		updateEndpointUrl();
+		ProdListDto resultado = webService.listaProduto(categoria);
+		return resultado;
+	}
+	
+	
 }
