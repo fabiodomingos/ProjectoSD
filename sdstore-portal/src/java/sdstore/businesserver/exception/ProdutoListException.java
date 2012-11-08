abb5ebefa54b9f69d9a4a8d511b7b8b0110cd00b
Mimoso <Mimoso@Mimoso-PC>
2012-11-08 12:56:47 +0000

    produto list exception portal

---
+++ sdstore-portal/src/java/sdstore/businesserver/exception/ProdutoListException.java
@@ -0,0 +1,13 @@
+package sdstore.businesserver.exception;
+
+public class ProdutoListException extends PortalException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ProdutoListException() {
+	}
+
+}
