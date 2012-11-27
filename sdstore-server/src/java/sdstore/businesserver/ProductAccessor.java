fc701ceab8857afa62704380f47e9cf5c23ffb63
Diogo <Diogo@Diogo-PC>
2012-11-27 17:35:32 +0000

    productaccessor

--- sdstore-server/src/java/sdstore/businesserver/ProductAccessor.java
+++ sdstore-server/src/java/sdstore/businesserver/ProductAccessor.java
@@ -5,7 +5,6 @@ import sdstore.businesserver.domain.Produto;
 import com.sleepycat.je.DatabaseException;
 import com.sleepycat.persist.EntityStore;
 import com.sleepycat.persist.PrimaryIndex;
-import com.sleepycat.persist.SecondaryIndex;
 
 public class ProductAccessor {
 	
