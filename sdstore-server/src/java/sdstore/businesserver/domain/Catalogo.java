0b99de1c40d64d5bfb13b16b7d439bdde28a0b52
Mimoso <Mimoso@172.20.20.35>
2012-11-29 12:41:25 +0000

    catalogo refactoring

--- sdstore-server/src/java/sdstore/businesserver/domain/Catalogo.java
+++ sdstore-server/src/java/sdstore/businesserver/domain/Catalogo.java
@@ -1,7 +1,6 @@
 package sdstore.businesserver.domain;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,7 +22,6 @@ public class Catalogo {
 	
 	private String nome;
 	private Double preco_portal;
-	//private static List<Produto> produtoMap;
 	private Integer quantidade_total;
 	
 	
@@ -56,70 +54,38 @@ public class Catalogo {
 		this.nome = name;
 		this.preco_portal = preco_portal;
 		this.quantidade_total = quantidade_total;
-		//produtoMap = new ArrayList<Produto>();
 		File dir = new File("/temp/"+name);
 		dir.mkdir();
-//		dados = new BaseDados(new File("/temp/"+name));
 		dados=new BaseDados(dir);
 	}
 	
 	public static void createCatalogo(String name){
-		Catalogo catalogo = Catalogo.catalogoMap.get(name);
-		if (catalogo != null) {
-//			throw new CatalogoNameException(name);
-		}
+//		Catalogo catalogo = Catalogo.catalogoMap.get(name);
 		Catalogo newCatalogo = new Catalogo(name, 0.1, 0);
 		Catalogo.catalogoMap.put(name, newCatalogo);
 	}
 //	
 	public static Catalogo getCatalogo(String name)  {
-		Catalogo catalogo = Catalogo.catalogoMap.get(name);
-		if (catalogo == null) {
-//			throw new CatalogoNameException(name);
-		}
+//		Catalogo catalogo = Catalogo.catalogoMap.get(name);
 		return Catalogo.catalogoMap.get(name);
 	}
 	
 	
 	public static Produto getProduto(String nome) throws ProdutoExistException{
-//		Integer controlo = 0;
-//		for(Produto p : produtoMap){
-//			if(p.getId().equals(nome)){
-//				controlo = 1;
-//				return p;
-//			}			
-//		}
-//		if(controlo==0){
-//			throw new ProdutoExistException(nome);
-//		}
-//		throw new ProdutoIdException(nome);
 		Produto p = dados.get(nome);
 		return p;
 	}
 	
 	public static List<Produto> getProdutoList() throws ProdutoListException{
-////		if(produtoMap.isEmpty()){
-////			throw new ProdutoListException();
-////		}
-////		return produtoMap;
-		
 		return dados.getCategoria();
 	}
 
 //	regista um produto no catalogo
 	public void registaProduto(Produto p,Double preco,Integer quantidade) {
-//		for(Produto prod : produtoMap){
-//			if(prod.getId().equals(p.getId())){
-////				throw new FornecedorProdutoExistsException(p.getId());
-//			}
-//		}
-//		p.setPreco(preco);
-//		p.setQuantidade(quantidade);
-//		produtoMap.add(p);
 		p.setQuantidade(quantidade);
 		p.setPreco(preco);
 		dados.run(p);
-		System.out.println("VERRRR A BASE DE DADOS: "+ dados);
+//		System.out.println("VERRRR A BASE DE DADOS: "+ dados);
 	}
 
 	public static String retiraProduto(String _codigo,Integer _quantidade) throws ProdutoExistException, QuantidadeException {
@@ -144,24 +110,6 @@ public class Catalogo {
 		}
 		dados.run(prod);
 		return resultado;
-//		for(Produto prod : produtoMap){
-//			if(prod.getId().equals(_codigo)){
-//				controlo = 1;
-//				if(prod.getQuantidade()<_quantidade){
-//					resultado="KO";
-//				}else{
-//					prod.setQuantidade(prod.getQuantidade()-_quantidade);
-//					resultado="OK";
-//				}
-//			}
-//		}
-//		if(resultado.equals("KO")){
-//			throw new QuantidadeException(_quantidade);
-//		}
-//		if(controlo==0){
-//			throw new ProdutoExistException(_codigo);
-//		}
-//		return resultado;
 	}
 	
 	
