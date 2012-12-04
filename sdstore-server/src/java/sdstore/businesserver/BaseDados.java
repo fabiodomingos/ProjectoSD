30b2e6f034c26b159d5463633874b24cc4c255bb
Diogo <Diogo@Diogo-PC>
2012-12-04 12:07:49 +0000

    classe BaseDados alterada com 2PC

--- sdstore-server/src/java/sdstore/businesserver/BaseDados.java
+++ sdstore-server/src/java/sdstore/businesserver/BaseDados.java
@@ -4,11 +4,16 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.transaction.xa.XAException;
+
 import sdstore.businesserver.domain.Produto;
 
 import com.sleepycat.je.DatabaseException;
 import com.sleepycat.je.Environment;
 import com.sleepycat.je.EnvironmentConfig;
+import com.sleepycat.je.Transaction;
+import com.sleepycat.je.XAEnvironment;
+import com.sleepycat.je.log.LogUtils.XidImpl;
 import com.sleepycat.persist.EntityCursor;
 import com.sleepycat.persist.EntityStore;
 import com.sleepycat.persist.StoreConfig;
@@ -16,9 +21,10 @@ import com.sleepycat.persist.StoreConfig;
 public class BaseDados {
 	
 	
-	private Environment env;
+	private XAEnvironment env;
 	private EntityStore store;
 	private ProductAccessor dao;
+	private Transaction txn;
 	
 	public BaseDados(File ficheiro) throws DatabaseException{
 		
@@ -26,7 +32,7 @@ public class BaseDados {
 		EnvironmentConfig envConfig = new EnvironmentConfig();
 		envConfig.setAllowCreate(true);
 		envConfig.setTransactional(true);
-		env = new Environment(ficheiro, envConfig);
+		env = new XAEnvironment(ficheiro, envConfig);
 
 		/* Open a transactional entity store. */
 		StoreConfig storeConfig = new StoreConfig();
@@ -70,6 +76,16 @@ public class BaseDados {
 		}
 		produtos.close();
 		return listaProdutos;
+	}
+	
+	
+	// PARTE PARA O 2PC ////////
+	
+	public void preparar(String xid) throws XAException{
+		txn = env.beginTransaction(null, null);
+		XidImpl imp = new XidImpl(1, xid.getBytes(), null);
+		env.setXATransaction(imp, txn);
+		env.prepare(imp);	
 	}
 	
 	
