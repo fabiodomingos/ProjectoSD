c4c6d24aee6c449f9e7bdf9c3a2bb117bfe06c4c
Diogo <Diogo@Diogo-PC>
2012-12-05 16:24:24 +0000

    retiraproduto com o commit

--- sdstore-server/src/java/sdstore/businesserver/service/RetiraProdutoService.java
+++ sdstore-server/src/java/sdstore/businesserver/service/RetiraProdutoService.java
@@ -9,16 +9,19 @@ public class RetiraProdutoService extends PortalService{
 	private String _codigo;
 	private Integer _quantidade;
 	private String result;
+	private String _tx;
 	
-	public RetiraProdutoService(String codigo,Integer quantidade){
+	public RetiraProdutoService(String codigo,Integer quantidade, String tx){
 		_codigo=codigo;
 		_quantidade=quantidade;
+		_tx=tx;
 	}
 
 	@Override
 	public final void dispatch() throws ProdutoExistException, QuantidadeException{
 		try{
 		result = Catalogo.retiraProduto(_codigo,_quantidade);
+		Catalogo.commit(_tx);
 		}catch(ProdutoExistException e){
 			throw new ProdutoExistException(_codigo);
 		}catch(QuantidadeException e){
