9a451e4eb7019b4bb2dea39cdddaf2b851d3996b
Mimoso <Mimoso@172.20.22.25>
2012-12-11 17:12:43 +0000

    portalwebservice

--- sdstore-server/src/java/sdstore/businesserver/service/PortalWebService.java
+++ sdstore-server/src/java/sdstore/businesserver/service/PortalWebService.java
@@ -3,6 +3,7 @@ package sdstore.businesserver.service;
 import javax.jws.HandlerChain;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import javax.transaction.xa.XAException;
 
 import sdstore.businesserver.exception.ProdutoExistException;
 import sdstore.businesserver.exception.ProdutoListException;
@@ -16,7 +17,7 @@ import sdstore.businesserver.service.dto.ProdutoListDto;
 public class PortalWebService {
 	
 	@WebMethod
-	public String RetiraProduto(String codigo,Integer quantidade,String tx) throws ProdutoExistException, QuantidadeException{
+	public String RetiraProduto(String codigo,Integer quantidade,String tx) throws ProdutoExistException, QuantidadeException,XAException{
 		RetiraProdutoService service = new RetiraProdutoService(codigo,quantidade,tx);
 		service.execute();
 		String result = service.getResultado();
@@ -51,8 +52,8 @@ public class PortalWebService {
 	
 	
 	@WebMethod
-	public String CanCommitService(String tx, String codigo, Integer quantidade){
-		CanCommitService service = new CanCommitService(tx,codigo,quantidade);
+	public String CanCommitService(String tx){
+		CanCommitService service = new CanCommitService(tx);
 		service.execute();
 		String resultado = service.getResultado();
 		return resultado;
@@ -62,5 +63,13 @@ public class PortalWebService {
 	public void AbortService(String tx){
 		AbortService service = new AbortService(tx);
 		service.execute();
+	}
+	
+	@WebMethod
+	public String CommitService(String tx){
+		CommitService service = new CommitService(tx);
+		service.execute();
+		String resultado = service.getResultado();
+		return resultado;
 	}
 }
