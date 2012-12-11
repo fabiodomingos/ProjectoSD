d734e8a2c35f952810e7d69c771a395ced8ff7a0
Mimoso <Mimoso@172.20.22.25>
2012-12-11 17:12:18 +0000

    cancommitservice

--- sdstore-server/src/java/sdstore/businesserver/service/CanCommitService.java
+++ sdstore-server/src/java/sdstore/businesserver/service/CanCommitService.java
@@ -1,26 +1,21 @@
 package sdstore.businesserver.service;
 
-import javax.transaction.xa.XAException;
+
 
 import sdstore.businesserver.domain.Catalogo;
 
 public class CanCommitService extends PortalService {
 	
 	private String _xid;
-	private String _codigo;
-	private Integer _quantidade;
 	private String _resultado;
 	
-	public CanCommitService(String xid, String codigo, Integer quantidade){
+	public CanCommitService(String xid){
 		_xid=xid;
-		_codigo = codigo;
-		_quantidade = quantidade;
-		
 	}
 	
 	@Override
 	public void dispatch(){
-		_resultado = Catalogo.prepare(_xid, _codigo, _quantidade);
+		_resultado = Catalogo.prepare(_xid);
 		
 	}
 	
