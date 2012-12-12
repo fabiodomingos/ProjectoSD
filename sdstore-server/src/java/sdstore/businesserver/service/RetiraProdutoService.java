a7d21208c4bac3d2db81d40aa14b5ce86a395911
Mimoso <Mimoso@Mimoso-PC>
2012-12-12 21:35:23 +0000

    retira refact

--- sdstore-server/src/java/sdstore/businesserver/service/RetiraProdutoService.java
+++ sdstore-server/src/java/sdstore/businesserver/service/RetiraProdutoService.java
@@ -1,21 +1,23 @@
 package sdstore.businesserver.service;
 
+import java.util.List;
+
 import javax.transaction.xa.XAException;
 
 import sdstore.businesserver.domain.Catalogo;
 import sdstore.businesserver.exception.ProdutoExistException;
 import sdstore.businesserver.exception.QuantidadeException;
+import sdstore.businesserver.service.dto.ProdutoDto;
 
 public class RetiraProdutoService extends PortalService{
 
-	private String _codigo;
-	private Integer _quantidade;
+
+	private List<ProdutoDto> _lista;
 	private String result;
 	private String _tx;
 	
-	public RetiraProdutoService(String codigo,Integer quantidade, String tx){
-		_codigo=codigo;
-		_quantidade=quantidade;
+	public RetiraProdutoService(List<ProdutoDto> lista, String tx){
+		_lista=lista;
 		_tx=tx;
 	}
 
@@ -23,12 +25,11 @@ public class RetiraProdutoService extends PortalService{
 	public final void dispatch() throws ProdutoExistException, QuantidadeException{
 		try{
 		
-		result = Catalogo.retiraProduto(_codigo,_quantidade,_tx);
-//		Catalogo.commit(_tx);
+		result = Catalogo.retiraProduto(_lista,_tx);
 		}catch(ProdutoExistException e){
-			throw new ProdutoExistException(_codigo);
+//			throw new ProdutoExistException(_codigo);
 		}catch(QuantidadeException e){
-			throw new QuantidadeException(_quantidade);
+//			throw new QuantidadeException(_quantidade);
 		}
 	}
 	
