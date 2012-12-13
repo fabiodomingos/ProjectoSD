bc660732a0d971b7c8915dd5a0a5581665797e5f
Diogo <Diogo@Diogo-PC>
2012-12-13 16:00:15 +0000

    Limpeza de comentarios

--- sdstore-server/src/java/sdstore/businesserver/exception/ProdutoListException.java
+++ sdstore-server/src/java/sdstore/businesserver/exception/ProdutoListException.java
@@ -7,20 +7,11 @@ public class ProdutoListException extends PortalException {
 	 */
 	private static final long serialVersionUID = 1L;
 
-//	private String codigo;
+
 
 	public ProdutoListException() {
-//		this.codigo = codigo;
-	}
 
-//	public String getCodigo() {
-//		return codigo;
-//	}
-//
-//	public void setCodigo(String codigo) {
-//		this.codigo = codigo;
-//	}
-//	
+	}
 	
 	
 }
